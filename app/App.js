2c0e8c2da781358a72214f47c3d2ba84ec74111b
hyeongjuSung <tjdgudwn1@gmail.com>
2021-05-14 14:08:44 +0900

    exam_4.8

--- app/App.js
+++ app/App.js
@@ -1,17 +1,36 @@
 import React, {Component} from 'react'
-import {View, StyleSheet} from 'react-native'
+import {StyleSheet, View, Text} from 'react-native'
 
-class App extends Component{
+class App extends Component {
     render(){
         return(
             <View style={styles.container}> 
-                <View style={styles.cardContainer}/>
+            <Example style={{borderWidth: 1}}>    
+               <Text>borderWidth: 1</Text>
+           </Example>
+           <Example style={{borderWidth: 3, borderLeftWidth: 0}}>    
+               <Text>borderWidth: 3, borderLeftWidth: 0</Text>
+           </Example>
+           <Example style={{borderWidth: 3, borderLeftColor: 'red'}}>    
+               <Text>borderWidth: 3, borderLeftColor: 'red'</Text>
+           </Example>
+           <Example style={{borderLeftWidth: 3}}>    
+               <Text>borderLeftWidth: 3</Text>
+           </Example>
+           <Example style={{borderWidth: 1, borderStyle: 'dashed'}}>    
+               <Text>borderWidth: 1, borderStyle: 'dashed'</Text>
+           </Example>
             </View>
         );
     }
 }
 // 여러 곳에서 사용할 경우를 대비해서 프로필 카드의 색상을 변수에 정의함
-const profileCardColor = 'dodgerblue'
+//const profileCardColor = 'dodgerblue'
+const Example = (props) => {
+    <View style={[styles.example, props.style]}>
+        {props.children}
+    </View>
+};
 
 const styles = StyleSheet.create({
     container: {    // 가장 바깥쪽 컴포넌트가 사용할 스타일
@@ -19,10 +38,13 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center'
     },
-    cardContainer: {    // 프로필 카드에서 사용할 스타일
-        backgroundColor: profileCardColor,
-        width: 300,
-        height: 400
+    // cardContainer: {    // 프로필 카드에서 사용할 스타일
+    //     backgroundColor: profileCardColor,
+    //     width: 300,
+    //     height: 400
+    // }
+    example: {
+        marginBottom: 15
     }
 });
 
