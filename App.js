cdcd156141cff471b151a58d0f83d45a586f6429
hyeongjuSung <tjdgudwn1@gmail.com>
2021-06-04 21:24:45 +0900

    exam_8.1

--- App.js
+++ App.js
@@ -1,74 +1,59 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import React, {createContext, Component} from 'react'
+import {StyleSheet, View, Text} from 'react-native'
 
- import React, {Component} from 'react';
- import type {Node} from 'react';
- import {
-   SafeAreaView,
-   ScrollView,
-   StatusBar,
-   StyleSheet,
-   Text,
-   useColorScheme,
-   View,
- } from 'react-native';
- 
- import {
-   Colors,
-   DebugInstructions,
-   Header,
-   LearnMoreLinks,
-   ReloadInstructions,
- } from 'react-native/Libraries/NewAppScreen';
- 
- //예제2.17 shouldComponentUpdate
- class App extends Component{
-   shouldComponentUpdate(nextProps, nextState) {
-     if(nextProps.name !== this.props.name) {
-       return true
-     }
-     return false
-   }
-   render() {
-     return <SomeComponent />
-   }
- }
- 
- //예제2.18 componentDidUpdate
- //리액트17 버전에서는 UNSAFE_componentWillUpdate로 변경 
- class App extends Component{
-   componentDidUpdate(prevProps, prevState) {
-     if(prevState.showToggled === this.state.showToggled) {
-          this.setState({
-         showToggled: !showToggled
-       })
-     }
-   }
-   render() {
-     return <SomeComponent />
-   }
- }
- 
- //예제2.19 componentWillUnmount
- class App extends Component{
-   handleClick() {
-     this._timeout = setTimeout(() => {
-       this.openWidget();
-     }, 2000);
-   }
-   componentWillUnmount() {
-     clearTimeout(this._timeout); 
-   }
-   render() {
-     return <SomeComponent 
-              handleClick={() => this.handleClick()} />
-   }
- }
- 
- 
- export default App;
+const ThemeContext = React.createContext()    
+class App extends Component {
+  state = { themeValue: 'light' }    
+  toggleThemeValue = () => {    
+    const value = this.state.themeValue === 'dark' ? 'light' : 'dark'    
+    this.setState({ themeValue: value })    
+  }    
+  render() {
+    return (
+      <ThemeContext.Provider    
+        value={{
+          themeValue: this.state.themeValue,
+          toggleThemeValue: this.toggleThemeValue
+        }}
+      >
+        <View style={styles.container}>
+          <Text>Hello World</Text>
+        </View>
+        <Child1 />
+      </ThemeContext.Provider>
+    );
+  }
+}
+
+const Child1 = () => <Child2 />    
+
+const Child2 = () => (    
+  <ThemeContext.Consumer>
+    {(val) => (
+        <View style={[styles.container, 
+                      val.themeValue === 'dark' && 
+                     { backgroundColor: 'black' }]}>
+          <Text style={styles.text}>Hello from Component2</Text>
+          <Text style={styles.text} 
+                onPress={val.toggleThemeValue}>
+              Toggle Theme Value
+          </Text>
+        </View>
+      )}
+  </ThemeContext.Consumer>
+)
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  text: {
+    fontSize: 22,
+    color: '#666'
+  }
+})
+
+export default App
